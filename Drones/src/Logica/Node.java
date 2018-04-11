9d046217c8ee67958fcc3714893773518e379442
charlie83Gs <carlosgomezsoza@gmail.com>
2018-04-11 00:27:07 -0600

    Primer version del codigo de grafo
    Codigo grafo

---
+++ Drones/src/Logica/Node.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Node {
+    private ArrayList<Node> adjacencys;
+    //el pesos se guarda en milisegundos
+    private ArrayList<Integer> weights;
+    
+    //permite ejecutar dijkstra
+    private boolean visited;
+    private int value;
+    
+    //ubicacion en el mapa, sirve para la interfaz
+    //puede que no sea necesario
+    private float x;
+    private float y;
+    
+    public Node(float p_x, float p_y){
+        x = p_x;
+        y = p_y;
+    }
+    
+    //funcion que le agrega una adjacencia al nodo
+    //conversion es la cantidad de milisegundos por unidad de distancia
+    public void addAdjacency(Node pNode, float pConversion){
+        adjacencys.add(pNode);
+        //calcula la distancia en tre ambos nodos
+        float distance = (float) Math.hypot(x-pNode.getX(), y - pNode.getY());
+        //calcula la cantidad de milisegundos que tardara el drone en recorrer la pista
+        weights.add((int)(distance*pConversion));
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float pX) {
+        this.x = pX;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float pY) {
+        this.y = pY;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean pVisited, int pValue) {
+        this.visited = pVisited;
+        this.value = pValue;
+    
+    }
+}
