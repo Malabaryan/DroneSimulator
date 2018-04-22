fe38cb276d08e4675b46240db9f3857d7e8e080c
Malabaryan <malabaryan@hotmail.com>
2018-04-22 14:48:01 -0500

    Update Timeline.java

--- Drones/src/generic/Timeline.java
+++ Drones/src/generic/Timeline.java
@@ -12,15 +12,37 @@ package Code;
 public class Timeline {
     
     
-    private int [] Line;
-    private int first;
-    private int last;
+    private int [] Line;                        /*Timeline*/
+    private int maxSpace;                       /*Lenght of array Line*/
+    private int maxByTime;                      /*Max int by unit of array Line*/
+    private TimelineController Controller;      /*Manage Time*/
 
-    public Timeline(int maxSpace) {
+    public Timeline(int pMaxSpace, TimelineController pController) {
         
         Line = new int[maxSpace * 2];
+        maxSpace = pMaxSpace;
     }
     
+    private boolean miliSecondIsFull(int pMiliSecond){
+        if(Line[pMiliSecond] >= maxByTime){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean addElementToMiliSecond(int pMiliSecond){
+        if(pMiliSecond < getactualTime())
+        if(!miliSecondIsFull(pMiliSecond)){
+            Line[pMiliSecond]++;
+            return true;
+        }
+            return false;
+    }
+    
+    private int getactualTime(){
+        Line[Controller.getActualTime()%maxSpace] = 0;
+        return Controller.getActualTime();
+    }
     
     
 }
