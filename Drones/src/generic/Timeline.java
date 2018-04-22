7b0f2c9e6cde196e03b55164c0e8d4163d250b15
Malabaryan <malabaryan@hotmail.com>
2018-04-22 00:53:17 -0500

    Create Timeline.java

---
+++ Drones/src/generic/Timeline.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Code;
+
+/**
+ *
+ * @author DilanHO
+ */
+public class Timeline {
+    
+    
+    private int [] Line;
+    private int first;
+    private int last;
+
+    public Timeline(int maxSpace) {
+        
+        Line = new int[maxSpace * 2];
+    }
+    
+    
+    
+}
