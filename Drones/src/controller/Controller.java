59ba92447ebc5f3ef190959df554a6e98926bd0c
Malabaryan <malabaryan@hotmail.com>
2018-04-22 21:26:24 -0500

    Update Controller.java

--- Drones/src/controller/Controller.java
+++ Drones/src/controller/Controller.java
@@ -15,13 +15,29 @@ public class Controller {
     
     private TimelineController TimeControl;
     
-    private void runSimulation(int HeightTrack, int WeightTrack, int SimulationTime, int CantidiadEstaciones){
+    private Station[] genStations(int weightMap, int heightMap, int stationCount){
+        int sizeOfMatrix = 6;
+        Random rand = new Random();
+        Station[] Stations = new Station[stationCount];
+        int xMatrix,yMatrix ;
+        for(Station tStation : Stations){
+            xMatrix = rand.nextInt(sizeOfMatrix);   /* 0 =< rand < sizeOfMatrix*/
+            yMatrix = rand.nextInt(sizeOfMatrix);   /* 0 =< rand < sizeOfMatrix*/
+
+            tStation = new Station(
+                    rand.nextInt(xMatrix +1 * weightMap/sizeOfMatrix) + xMatrix * weightMap/sizeOfMatrix,
+                    rand.nextInt(yMatrix +1 * heightMap/sizeOfMatrix) + yMatrix * heightMap/sizeOfMatrix);
+            tStation.setLineIn(new Timeline(5,TimeControl));
+        }
+        return Stations;
+    }
+    
+
+    public void runSimulation(int HeightTrack, int WeightTrack, int SimulationTime, int stationCount){
+        
+        Station[] Stations = genStations(500,500,stationCount);
         
         /*
-        -Create Stations
-        -For each one{
-            new Timeline(TimeControl);
-        }
         - Dejar listas las estaciones
         - Correr los dijsktras
         - Correr los aleatorios para las pistas
