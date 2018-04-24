c69b6fa05e34dc6fce7c5d8ef937e1fb26878615
Malabaryan <malabaryan@hotmail.com>
2018-04-24 00:30:42 -0600

    Add files via upload

--- Drones/src/helper/MapHelper.java
+++ Drones/src/helper/MapHelper.java
@@ -5,8 +5,8 @@
  */
 package helper;
 
-import Logica.Station;
-import generic.Node;
+import controller.Station;
+import code.GraphNode;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -23,11 +23,11 @@ public class MapHelper {
     
     
     //for debuging
-    public static void printOptimalRoutes(Map<Node<Station>,ArrayList<Node<Station>>> routes, ArrayList<Node<Station>> nodes){
-        ArrayList<Node<Station>> list;
-        for(Node<Station> station : nodes){
+    public static void printOptimalRoutes(Map<GraphNode<Station>,ArrayList<GraphNode<Station>>> routes, ArrayList<GraphNode<Station>> nodes){
+        ArrayList<GraphNode<Station>> list;
+        for(GraphNode<Station> station : nodes){
             list = routes.get(station);
-            for(Node<Station> node : list){
+            for(GraphNode<Station> node : list){
                 System.out.print(node.getContent().getX() + ", " + node.getContent().getY() + " >< ");
             }
             System.out.println("---");
