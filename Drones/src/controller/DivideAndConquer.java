fa72fecf466f7a5458e51979f70364821df5780e
Malabaryan <malabaryan@hotmail.com>
2018-04-28 10:01:32 -0500

    Update DivideAndConquer.java

--- Drones/src/controller/DivideAndConquer.java
+++ Drones/src/controller/DivideAndConquer.java
@@ -6,6 +6,9 @@
 package controller;
 
 import code.Graph;
+import code.GraphNode;
+import controller.Station;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,8 +19,37 @@ public class DivideAndConquer extends SolutionAlgorithm{
     @Override
     public void solve(int dronesByBlock, Graph<Station> GraphMap, int DronesCount, int stationCount, int simulationTime, int blockSize) {
         
+    int trips = DronesCount / dronesByBlock;
     
-    
+    ArrayList<GraphNode<Station>>[] Route;
+    Route = new ArrayList[trips];
+
+    for(int cantTripsLeft = 0;trips < cantTripsLeft; cantTripsLeft++){
+        Route[trips] = GraphMap.getNode(22222222).getContent().getOptimalRoutes().get(
+                                                    GraphMap.getNode( 222222 ));
     }
     
+    int between = Route.length/2;
+    ArrayList<GraphNode<Station>>[] RouteLeft = new ArrayList[between];
+    ArrayList<GraphNode<Station>>[] RouteRight = new ArrayList[between];
+    System.arraycopy(Route,0, RouteLeft,0,between);
+    System.arraycopy(Route,0, RouteLeft,0,between);
+    divideAux(RouteLeft, RouteRight, Route);
+    }
+    
+    public void divideAux(ArrayList<GraphNode<Station>>[] Left, ArrayList<GraphNode<Station>>[] Right, ArrayList<GraphNode<Station>>[] Route){
+        int between = Route.length/2;
+        if(between < 2){
+            
+        }
+        else{
+            ArrayList<GraphNode<Station>>[] RouteLeft = new ArrayList[between];
+            ArrayList<GraphNode<Station>>[] RouteRight = new ArrayList[between];
+            System.arraycopy(Route,0, RouteLeft,0,between);
+            System.arraycopy(Route,0, RouteLeft,0,between);
+            divideAux(RouteLeft, RouteRight, Route);
+        }
+        
+            
+    }
 }
